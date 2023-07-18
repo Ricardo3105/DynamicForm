c97eb0b2aeaba5147696a6810a09b3e0bdb54d14
Ricardo Arias <ricardoarias.2012@hotmail.com>
2023-07-17 22:22:43 -0500

    Se agregan comentarios en español

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -7,22 +7,22 @@ export default function Introduction() {
                 <Row className="justify-content-md-center">
                     <Col xs={12} md={8}>
                         <h1 className="pl-2 text-center">
-                            Welcome to my Dynamic Form {" "}
-                            <span className="pt-2 pb-4">that you will do everything that you want</span>
+                            Bienvenido a la academia para desarrollo personal {" "}
+                            <span className="pt-2 pb-4">¿Qué quieres aprender?</span>
                         </h1>
                         <h4 className="d-flex container-actions mt-5">
-                            Hi, I'm Iván Devia, im 38 years old and i like make programs an develop with js and JAVA. I like play BaseBall y play drums.
+                            En esta academia puedes aprender desde desarrollo web hasta montar tu propia Start-Up de tennología, aventurate con nosotros y descubre todo tu potencial
                         </h4>
                         <div className="d-flex container-actions mt-5">
                             <div className="container-buttons">
                                 <Button
                                     className="p-3"
                                     onClick={() => window.location='#services'}
-                                >Get in touch</Button>
-                                <Button className="p-3">Get in touch</Button>
+                                >Nuestros Cursos</Button>
+                                <Button className="p-3">Nuestros Cursos</Button>
                             </div>
                             <div className="btn-start-create">
-                                <a href="#dynamic-form">Start to create</a>
+                                <a href="#dynamic-form">Cuentanos sobre ti</a>
                             </div>
                         </div>
                     </Col>
